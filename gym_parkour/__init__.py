1b4525c84dcb6bb4a7d9f3e3369c53cca38136a7
stlukyanenko <stlukyanenko@gmail.com>
2019-12-23 07:19:10 +0100

    clearing up our code for the starting-kit

---
+++ gym_parkour/__init__.py
@@ -0,0 +1,12 @@
+from gym.envs.registration import register
+
+print('registering env')
+register(
+    id='ParkourChallenge-v0',
+    entry_point='gym_parkour.envs.parkour_gym:ParkourGym',
+)
+
+register(
+    id='ParkourBiped-v0',
+    entry_point='gym_parkour.envs.parkour_gym_biped:ParkourGymBiped'
+)
---
+++ gym_parkour/envs/parkour_robot.py
@@ -0,0 +1,40 @@
+import os
+import pybullet
+import numpy as np
+from pybulletgym.envs.roboschool.robots.robot_bases import XmlBasedRobot
+from abc import ABC, abstractmethod
+
+
+class ParkourRobot(ABC):
+    def __init__(self, target_position_xy):
+        self.target_position_xy = target_position_xy
+        power = 0.41
+        self.power = power
+
+    @abstractmethod
+    def reset(self, bullet_client):
+        pass
+
+    def apply_action(self, a):
+        assert (np.isfinite(a).all())
+        for n, j in enumerate(self.ordered_joints):
+            j.set_motor_torque(self.power * j.power_coef * float(np.clip(a[n], -1, +1)))
+
+    @abstractmethod
+    def calc_state(self):
+        # return the robot state. This is what is returned by gym.step as state
+        pass
+
+    @abstractmethod
+    def calc_reward(self, action, ground_ids):
+        pass
+
+    @abstractmethod
+    def is_alive(self):
+        # return True if robot is still alive. Otherwise False
+        # if False is returned the done flag will be set
+        pass
+
+    def calc_potential(self):
+        # necessary method for BulletBaseEnv
+        return 0
