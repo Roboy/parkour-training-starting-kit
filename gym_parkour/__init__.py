6955c025337e95fd66c5029ac955d493d3b03705
stlukyanenko <stlukyanenko@gmail.com>
2019-12-23 07:23:15 +0100

    clearing up our code for the starting-kit

--- gym_parkour/__init__.py
+++ gym_parkour/__init__.py
@@ -5,8 +5,3 @@ register(
     id='ParkourChallenge-v0',
     entry_point='gym_parkour.envs.parkour_gym:ParkourGym',
 )
-
-register(
-    id='ParkourBiped-v0',
-    entry_point='gym_parkour.envs.parkour_gym_biped:ParkourGymBiped'
-)
